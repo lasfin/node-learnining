4ebd1fe6bdb6c92f0317d4b1bd074fb02aa5e06f
lasfin <evgenij-kopica@yandex.ru>
2016-01-31 16:50:57 +0300

    can add a post now

--- 05-node-blog-system/routes/posts.js
+++ 05-node-blog-system/routes/posts.js
@@ -5,17 +5,21 @@ var db = require('monk')('localhost/nodeblog');
 
 
 router.get('/add', function(req, res, next) {
-    res.render('addpost', {
-       'title': 'Add post'
+    var categories = db.get('categories');
+    categories.find({}, {}, function(err, categories) {
+        res.render('addpost', {
+            title: 'Add post',
+            categories: categories
+        });
     });
 });
 
-router.post('add', function(req, res, next){
+router.post('/add', function(req, res, next){
    // get form values
-    var title = req.form.title;
-    var category = req.form.category;
-    var body = req.form.body;
-    var author = req.form.author;
+    var title = req.body.title;
+    var category = req.body.category;
+    var body = req.body.body;
+    var author = req.body.author;
     var date = new Date();
 
     if (req.files.mainimage) {
@@ -30,7 +34,38 @@ router.post('add', function(req, res, next){
     }
 
     // form validation
+    req.checkBody('title', 'title field is required').notEmpty();
+    req.checkBody('body', 'body field is required').notEmpty();
 
+    // check errors
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render({
+            errors: erros,
+            title: title,
+            body: body
+        })
+    } else {
+        var posts = db.get('posts');
+        // submit to db
+        posts.insert({
+            title: title,
+            body: body,
+            category: category,
+            date: date,
+            author: author,
+            mainimage: mainImageName
+        }, function(err, post) {
+            if (err) {
+                res.send('There was an issue submitting the post')
+            } else {
+                req.flash('success', 'Post submitted');
+                res.location('/');
+                res.redirect('/');
+            }
+        })
+    }
 
 
 });
