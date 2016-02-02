ff2aa387258a0b59fb180170b6d51eb87484af84
lasfin <evgenij-kopica@yandex.ru>
2016-02-03 00:54:43 +0300

    add comments and show them

--- 05-node-blog-system/routes/posts.js
+++ 05-node-blog-system/routes/posts.js
@@ -51,7 +51,7 @@ router.post('/add', (req, res, next) => {
     req.checkBody('body', 'body field is required').notEmpty();
 
     // check errors
-    var errors = req.validationErrors();
+    let errors = req.validationErrors();
 
     if (errors) {
         res.render('addpost', {
@@ -76,6 +76,56 @@ router.post('/add', (req, res, next) => {
                 req.flash('success', 'Post submitted');
                 res.location('/');
                 res.redirect('/');
+            }
+        })
+    }
+
+
+});
+
+
+router.post('/addcomment', (req, res, next) => {
+    // get form values
+    let name = req.body.name;
+    let email = req.body.email;
+    let body = req.body.body;
+    let postid = req.body.postid;
+    let commentDate = new Date();
+
+    // form validation
+    req.checkBody('name', 'name field is required').notEmpty();
+    req.checkBody('body', 'body field is required').notEmpty();
+    req.checkBody('email', 'e-mail field is required').notEmpty();
+    req.checkBody('email', 'e-mail field not formatted correctly').isEmail();
+
+    // check errors
+    let errors = req.validationErrors();
+
+    if (errors) {
+        let posts = db.get('post');
+        posts.findById(postid, ()=>{
+            res.render('showpost', {
+                errors,
+                posts
+            })
+        });
+    } else {
+        let comment = {name, email, body, commentDate};
+        let posts = db.get('posts');
+        // submit to db
+        posts.update({
+            '_id': postid
+        }, {
+            $push: {
+                'comments': comment
+            }
+        }, (err, document) => {
+            if (err) {
+                throw err;
+            } else {
+                req.flash('success', 'comment added');
+                res.location('/posts/show/' + postid);
+                res.redirect('/posts/show/' + postid);
             }
         })
     }
